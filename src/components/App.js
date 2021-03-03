27a45ee9afc849515a059786efb64cb254e266b4
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-02 19:34:02 -0500

    built react app without CRA.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import test from './../img/test.svg'
+
+const App = () => {
+	return (
+		<>
+			<h1>Hello, World!</h1>
+			<img src={test} alt="Just a test image" />
+		</>
+	)
+}
+
+export default App
